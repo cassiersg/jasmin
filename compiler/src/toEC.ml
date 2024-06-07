cf5f5280f71ac2d63dd9b0486c5a035ec5bf8e23
Gaëtan Cassiers <gaetan.cassiers@uclouvain.be>
2024-06-07 22:58:22 +0200

    Parentheses inside randombytes

--- compiler/src/toEC.ml
+++ compiler/src/toEC.ml
@@ -1351,7 +1351,7 @@ let pp_prog pd asmOp fmt model globs funcs arrsz warrsz randombytes =
         syscall_mod_sig
         (pp_list "@ " pp_randombytes_decl) (Sint.elements !(env.randombytes));
       let pp_randombytes_proc fmt n =
-        Format.fprintf fmt "proc randombytes_%i (a:W8.t %a.t) : W8.t %a.t = {@   a <$ @[dmap %a.darray@ (fun a => %a.init (fun i => %a.get8 a i))@];@   return a;@ }"
+        Format.fprintf fmt "proc randombytes_%i (a:W8.t %a.t) : W8.t %a.t = {@   a <$ @[(dmap %a.darray@ (fun a => (%a.init (fun i => (%a.get8 a i)))))@];@   return a;@ }"
           n (pp_Array env) n (pp_Array env) n (pp_WArray env) n 
           (pp_Array env) n (pp_WArray env) n
       in
